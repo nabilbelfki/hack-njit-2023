753a8ce6494ea374eea7f857d3e463be2e1e32e9
Nabil Belfki <nabilbelfki@gmail.com>
2023-11-04 17:24:08 -0400

    adding project

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>
+            Hack NJIT
+        </title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="ship.js"></script>
+        <link rel="stylesheet" href="ship.css">
+    </head>
+    <body>
+        <div id="scoreboard">0</div>
+        <img id="ship" src="Ship.png" alt="ship with containers">
+        <img id="coin" src="Coin.png" alt="coin with dollar sign">
+        <img id="coin" src="Spinner.png" alt="spinner with spikes, be careful this could destroy your ship">
+    </body>
+</html>
